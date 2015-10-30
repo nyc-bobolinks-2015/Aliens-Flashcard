d74b6fa4b0db63fcc398fe069b29d43bf8883837
Nathan Luo <naipton@gmail.com>
2015-10-30 14:40:34 -0400

    New method for parsing original file

--- temp.rb
+++ temp.rb
@@ -1,11 +1,35 @@
 require 'pry'
 require 'csv'
 
-def load_deck(filename)
-    CSV.foreach(filename, headers: true, col_sep: "$", header_converters: :symbol) do |row|
-      card = Flashcard.new(row)
-      @deck.add_card(card)
-    end
+class Flashcard
+  attr_reader :definition, :answer
+  def initialize(args = {})
+    @definition = args[:definition]
+    @answer = args[:answer]
+  end
 end
 
-load_deck
+$testing = []
+def load_deck(filename)
+  counter = 1
+  args = {}
+  File.foreach(filename, col_sep: "\n") do |csv_line|
+    # binding.pry
+    if counter == 1
+      args[:definition] = csv_line.chomp
+      counter += 1
+    elsif counter == 2
+      args[:answer] = csv_line.chomp
+      counter += 1
+    elsif counter == 3
+      counter = 1
+      card = Flashcard.new(args)
+      $testing << card
+    end
+  end
+end
+
+load_deck('flashcard_samples_redux.txt')
+
+binding.pry
+puts ""
